f09b6fd3d763a22733932076f85863a14fb6ed17
GajicM <mgajic9821rn@raf.rs>
2025-03-03 23:45:59 +0100

    deleted api key

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class SearchService {
-  yt_key = 'AIzaSyCINhVbBbpKleBpZjvBfFQRMrlhfcwMqAA';
+  yt_key = '';
   constructor(private httpClient: HttpClient) {}
 
   search(value: string): Observable<string[]> {
